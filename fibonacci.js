3619ad51c74efb76e2bcfdc8f0768d922954e6fd
VrushaliChanna <45230044+vrushalichanna@users.noreply.github.com>
2024-01-22 13:35:59 +0530

    Create fibonacci.js
    Fibonacci Sequence program

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+function fibonacci(n){
+const fib = [0,1]
+for(let i = 2; i< n; i++) {
+	fib[i] = fib[i - 1] + fib[i - 2]
+}
+return fib
+}
+
+console.log(fibonacci(1))
+console.log(fibonacci(2))
+console.log(fibonacci(3))
+console.log(fibonacci(4))
+console.log(fibonacci(5))
+console.log(fibonacci(6))
+console.log(fibonacci(8))
+
+//Big O = O[n]
