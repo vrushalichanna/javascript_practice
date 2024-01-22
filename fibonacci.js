2ea3e048614b85cda3297b2006921a558c395344
VrushaliChanna <45230044+vrushalichanna@users.noreply.github.com>
2024-01-22 15:42:26 +0530

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -15,3 +15,21 @@ console.log(fibonacci(6))
 console.log(fibonacci(8))
 
 //Big O = O[n]
+
+//recursive approch solution
+function recursiveFibonacci(n){
+if(n<2) {
+	return n
+} 
+
+return recursiveFibonacci(n-1) + recursiveFibonacci(n-2)
+}
+
+console.log(recursiveFibonacci(1))
+console.log(recursiveFibonacci(2))
+console.log(recursiveFibonacci(4))
+console.log(recursiveFibonacci(5))
+console.log(recursiveFibonacci(6))
+console.log(recursiveFibonacci(8))
+
+//Big O = O[1]
