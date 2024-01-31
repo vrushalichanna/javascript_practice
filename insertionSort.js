6d6b0b2d982aec6f160836e034e4bb18f319e61a
VrushaliChanna <45230044+vrushalichanna@users.noreply.github.com>
2024-01-31 15:26:44 +0530

    Create insertionSort.js

---
+++ insertionSort.js
@@ -0,0 +1,36 @@
+function insertionSortASC(arr) {
+
+    for (let i = 1; i < arr.length; i++) {
+        let numberToInsert = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > numberToInsert) {
+            arr[j + 1] = arr[j]
+            j = j - 1;
+        }
+        arr[j + 1] = numberToInsert
+    }
+}
+
+function insertionSortDSC(arr) {
+
+    for (let i = 1; i < arr.length; i++) {
+        let numberToInsert = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] < numberToInsert) {
+            arr[j + 1] = arr[j]
+            j = j - 1;
+        }
+        arr[j + 1] = numberToInsert
+    }
+}
+
+
+const arr = [8,20,-2,4,-6]
+insertionSortASC(arr);
+console.log(arr) // [-6, -2, 4, 8, 20]
+insertionSortDSC(arr);
+console.log(arr) // [20, 8, 4, -2, -6]
+
+
+
+//Big O = O[n^2]
